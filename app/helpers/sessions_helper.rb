bb6096da8e818286c0ba0438d356abed8dee4c9c
Eric Mayfield <eric@visiblycreative.com>
2019-03-22 10:35:37 -0400

    Done.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+    def if_user_logged_in
+        if session[:name]
+            render 'form'
+        else
+            link_to "Login", login_path
+        end
+    end
 end
